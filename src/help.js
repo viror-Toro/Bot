3016eabcb0584129b0699491354609c38b0b1c10
viror-Toro <virortoro@gmail.com>
2021-05-11 02:18:34 -0500

    First commit

---
+++ src/help.js
@@ -0,0 +1,340 @@
+const help = (prefix, sender, pushname, time) => {
+
+	return `
+ <═════════════════════>
+   *🇽 🇦 🇳 🇩 🇦 🇴  🇧 🇴 🇹  *
+ <═════════════════════>
+ 
+ ╭╮╮╱▔▔▔▔╲╭╭╮ 
+ ╰╲╲▏▂╲╱▂▕╱╱╯
+ ┈┈╲▏▇▏▕▇▕╱┈┈
+ ┈┈╱╲▔▕▍▔╱╲┈┈ 
+ ╭╱╱▕╋╋╋╋▏╲╲╮
+ ╰╯╯┈╲▂▂╱┈╰╰╯
+💪 𝙏𝙚𝙣𝙩𝙖 𝙥𝙚𝙞𝙩𝙖𝙧 𝙥𝙧𝙖 𝙩𝙪 𝙫𝙚𝙧 𝙝𝙚𝙝𝙚 💪
+     𝚅𝚊𝚒 𝚃𝚎𝚗𝚝𝚊𝚛 ${pushname}?
+               ☞︎︎︎Bᴏᴛ✌︎︎Mᴇɴᴜ☜︎︎︎
+           Dᴏɴᴏ:✞︎Mᴇʟɪᴏᴅᴀs✞︎
+       Cʀɪᴀᴅᴏʀ:Cᴀᴜss
+┌─────────❶.❷────────
+║〘 INFORMAÇÕES 〙
+║
+╠✰ *Super Xandão parsa, Peita ou Respeita* 
+║
+╠✰ 𝐃𝐎𝐍𝐎:  ⃬⃗Mᴇʟɪᴏᴅᴀs (Bᴀsᴇ ᴅᴏ Cᴀᴜss ǫᴜᴇ ᴇ ᴅᴏ Dᴀʀᴋ) 
+║
+╠✰ Quer falar com o Causs? acessa: https://wa.me/5547992091566
+║
+╠✰ Nmr do Dono: *wa.me/5589981246187*
+║
+╠✰ Seu link do zap parsa: wa.me/${sender.split("@")[0]}
+║
+╠✰ 𝐒𝐓𝐀𝐓𝐔𝐒: BRABO COMO SEMPRE💪⚡
+║
+╠✰ Velocidade: ${process.uptime()}
+║
+╠Data e hora: ${time}
+║
+╠══Xandao ta trabalhando à: ${kyun(uptime)}
+╠══༒〘 COMANDOS NOVOS 〙༒︎ ︎══
+║
+║☯︎ ➪ *supergay
+║☯︎ ➪ *gostosa
+║☯︎ ➪ *casal
+║☯︎ ➪ ${prefix}cornos
+║☯︎ ➪ ${prefix}
+║
+╠══༒︎〘 MENU 〙༒︎══
+║
+║☯︎ ➪ **sticker*
+║☯︎ ➪ **toimg*
+║☯︎ ➪ **darkjokes (memes aleatórios)*
+║☯︎ ➪ **memeindo*
+║☯︎ ➪ **tts*
+║☯︎ ➪ **lolih [on]*
+║☯︎ ➪ **nsfwloli [off]*
+║☯︎ ➪ **url2img*
+║☯︎ ➪ **leens [na legenda]*
+║☯︎ ➪ **wait [na legenda]*
+║☯︎ ➪ **setprefix*
+║
+╠══༒︎〘 OUTROS 〙༒︎══
+║
+║☯︎ ➪ **linkgp*
+║☯︎ ➪ **simih [1/0]*
+║☯︎ ➪ **marcar*
+║☯︎ ➪ **add [@]*
+║☯︎ ➪ **banir [@]*
+║☯︎ ➪ **promover [@]*
+║☯︎ ➪ **rebaixar*
+║☯︎ ➪ **admins*
+║☯︎ ➪ **marcar2*
+║☯︎ ➪ **bc [texto]* (ele faz uma ™)
+║☯︎ ➪ **marcar3*
+║☯︎ ➪ **bloqueados*
+║☯︎ ➪ **bloquear [@]*
+║☯︎ ➪ **desbloquear [@]*
+║☯︎ ➪ **limpar*
+║☯︎ ➪ **bc [ *texto* ]*
+║☯︎ ➪ **bemvindo [1/0]*
+║☯︎ ➪ **clonar [@]*
+║☯︎ ➪ **help1*
+║☯︎ ➪ **dono*
+║☯︎ ➪ **owner*
+║☯︎ ➪ **tts [texto]*
+║☯︎ ➪ **setnome*
+║☯︎ ➪ **termux*
+║☯︎ ➪ **setfoto*
+║☯︎ ➪ **grupoinfo*
+║☯︎ ➪ **ytmp4*
+║☯︎ ➪ **bomdia*
+║☯︎ ➪ **boanoite*
+║☯︎ ➪ **marcar*
+║☯︎ ➪ **marcar2*
+║☯︎ ➪ **marcar3*
+║
+╠══༒︎〘 IMAGENS 〙༒︎══
+║
+║☯︎ ➪ **loli* [off]
+║☯︎ ➪ **loli1*
+║☯︎ ➪ **hentai*
+║☯︎ ➪ **dono*
+║☯︎ ➪ **porno*
+║☯︎ ➪ **boanoite*
+║☯︎ ➪ **bomdia*
+║☯︎ ➪ **boatarde*
+║☯︎ ➪ **mia [aleatórias]*
+║☯︎ ➪ **rize [aleatórias]*
+║☯︎ ➪ **minato [aleatórias]*
+║☯︎ ➪ **boruto [aleatórias]*
+║☯︎ ➪ **hinata [aleatórias]*
+║☯︎ ➪ **sasuke [aleatórias]*
+║☯︎ ➪ **sakura [aleatórias]*
+║☯︎ ➪ **naruto [aleatórias]*
+║☯︎ ➪ **meme*   
+║☯︎ ➪ **lofi*
+║☯︎ ➪ **malkova*
+║☯︎ ➪ **canal*
+║☯︎ ➪ **nsfwloli1*
+║☯︎ ➪ **reislin*
+║
+╠══༒︎〘 INTELIGÊNCIA 〙༒︎══
+║
+║☯︎ ➪ **simih 1 (para ativar)*
+║☯︎ ➪ **simih 0 (para desativar)*
+║ **simi (sua mensagem)*
+║
+╠══༒︎〘 EM PRODUÇÃO 〙༒︎══
+║
+║☯︎ ➪ **gado*
+║☯︎ ➪ **dbz*
+║☯︎ ➪ **gtts loli*
+║☯︎ ➪ **hidegp*
+║
+╠══༒︎〘 SÓ PREMIUM 〙༒︎══
+║
+║☯︎ ➪ **dado*
+║☯︎ ➪ **cekvip*
+║☯︎ ➪ **premiumlist*
+║☯︎ ➪ **delete*
+║☯︎ ➪ **modapk*
+║☯︎ ➪ **indo10*
+║☯︎ ➪ **daftarvip [para virar Premium]*
+║☯︎ ➪ **qrcode*
+║☯︎ ➪ **chentai*
+║☯︎ ➪ **gcpf*
+║☯︎ ➪ **gbin*
+║☯︎ ➪ **pack*
+║☯︎ ➪ **gpessoa*
+║
+╠══༒︎〘 GRUPO 〙༒︎══
+║
+║☯︎ ➪ **banir*
+║☯︎ ➪ **leveling [on/off]*
+║☯︎ ➪ **level*
+║☯︎ ➪ **add*
+║☯︎ ➪ **promover*
+║☯︎ ➪ **setfoto [na legenda]*
+║☯︎ ➪ **setname [texto]*
+║☯︎ ➪ **rebaixar*
+║☯︎ ➪ **admins*
+║☯︎ ➪ **marcar*
+║☯︎ ➪ **marcar2*
+║☯︎ ➪ **marcar3*
+║☯︎ ➪ **bemvindo [1/0]*
+║☯︎ ➪ **grupoinfo*
+║☯︎ ➪ **bomdia*
+║☯︎ ➪ **boatarde*
+║☯︎ ➪ **boanoite*
+║☯︎ ➪ **setdesc*
+║☯︎ ➪ **bug [sua mensagem]*
+║
+╠══༒︎〘 ESPECIFICO DO BOT 〙༒︎══
+║
+║☯︎ ➪ **bug [sua mensagem]*
+║☯︎ ➪ **clonar [@]*
+║☯︎ ➪ **dono*
+║☯︎ ➪ **ping [ver velocidade do bot]*
+║☯︎ ➪ **termux*
+║☯︎ ➪ **gay [@]*
+║☯︎ ➪ **wame*
+║☯︎ ➪ **map (nome)*
+║☯︎ ➪ **setppbot (marque uma img)*
+║☯︎ ➪ **pinterest (nome)*
+║☯︎ ➪ **desligar (so para o dono)*
+║☯︎ ➪ **timer*
+║
+╠══༒︎〘 MAIS ALGUNS 〙༒︎══
+║
+║☯︎ ➪ **neko*
+║☯︎ ➪ **ttp [texto]*
+║☯︎ ➪ **testime*
+║☯︎ ➪ **tomp3*
+║☯︎ ➪ **modoanime [on/off]*
+║☯︎ ➪ **modonsfw [on/off]*
+║☯︎ ➪ **happymod [jogo/app]*
+║☯︎ ➪ **rize*
+║☯︎ ➪ **ytsearch*
+║☯︎ ➪ **moddroid [jogo/app]*
+║☯︎ ➪ **xvideos [titulo]**
+║☯︎ ➪ **nomegp*
+║☯︎ ➪ **darkjokes (memes aleatórios)*
+║☯︎ ➪ **animecry*
+║☯︎ ➪ **gay1*
+║☯︎ ➪ **next*
+║☯︎ ➪ **alerta*
+║☯︎ ➪ **belle [img aleatórias]*
+║☯︎ ➪ **pronomeneu [texto]*
+║☯︎ ➪ **hobby*
+║
+╠══༒︎〘 COMANDOS DE VOZ 〙༒︎══
+║
+║☯︎ ➪ **ola*
+║☯︎ ➪ **bv*
+║☯︎ ➪ **tchau*
+║☯︎ ➪ **bem*
+║☯︎ ➪ **a*
+║☯︎ ➪ **fdp*
+║☯︎ ➪ **onich*
+║☯︎ ➪ **beat1*
+║☯︎ ➪ **beat2*
+║☯︎ ➪ **beat3*
+║☯︎ ➪ **beat4*
+║☯︎ ➪ **beat5*
+║☯︎ ➪ **beat6*
+║☯︎ ➪ **beat7*
+║☯︎ ➪ **beat8*
+║☯︎ ➪ **beat9*
+║☯︎ ➪ **beat10*
+║☯︎ ➪ **beat11*
+║☯︎ ➪ **beat12*
+║☯︎ ➪ **beat13*
+║☯︎ ➪ **beat14*
+║☯︎ ➪ **beat15*
+║☯︎ ➪ **beat16*
+║☯︎ ➪ **beat17*
+║☯︎ ➪ **beat18*
+║☯︎ ➪ **beat19*
+║☯︎ ➪ **beat20*
+║☯︎ ➪ **beat21*
+║☯︎ ➪ **beat22*
+║☯︎ ➪ **beat23*
+║☯︎ ➪ **beat24*
+║☯︎ ➪ **beat25*
+║☯︎ ➪ **trap1*
+║☯︎ ➪ **trap2*
+║☯︎ ➪ **trap3*
+║☯︎ ➪ **trap4*
+║☯︎ ➪ **trap5*
+║☯︎ ➪ **trap6*
+║☯︎ ➪ **trap7*
+║☯︎ ➪ **trap8*
+║☯︎ ➪ **trap9*
+║☯︎ ➪ **trap10*
+║☯︎ ➪ **trap11*
+║☯︎ ➪ **trap12*
+║☯︎ ➪ **trap13*
+║☯︎ ➪ **trap14* *[A MELHOR MÚSICA✓]*
+║☯︎ ➪ **trap15*
+║☯︎ ➪ **trap16*
+║☯︎ ➪ **trap17*
+║☯︎ ➪ **trap18*
+║☯︎ ➪ **trap19*
+║☯︎ ➪ **trap20*
+║☯︎ ➪ **trap21*
+║☯︎ ➪ **trap22*
+║☯︎ ➪ **trap23*
+║☯︎ ➪ **trap24*
+║☯︎ ➪ **trap25*
+║☯︎ ➪ **roc1*
+║☯︎ ➪ **roc2*
+║☯︎ ➪ **roc3*
+║☯︎ ➪ **roc4*
+║☯︎ ➪ **roc5*
+║☯︎ ➪ **roc6*
+║☯︎ ➪ **roc7*
+║☯︎ ➪ **roc8*
+║☯︎ ➪ **roc9*
+║☯︎ ➪ **roc10*
+║☯︎ ➪ **roc11*
+║☯︎ ➪ **roc12*
+║☯︎ ➪ **roc13*
+║☯︎ ➪ **roc14*
+║☯︎ ➪ **roc15*
+║☯︎ ➪ **roc16*
+║☯︎ ➪ **roc17*
+║☯︎ ➪ **roc18*
+║☯︎ ➪ **roc19*
+║☯︎ ➪ **roc20*
+║☯︎ ➪ **roc21*
+║☯︎ ➪ **roc22*
+║☯︎ ➪ **roc23*
+║☯︎ ➪ **roc24*
+║☯︎ ➪ **roc25*
+║☯︎ ➪ **rap1*
+║☯︎ ➪ **rap2*
+║☯︎ ➪ **rap3*
+║☯︎ ➪ **rap4*
+║☯︎ ➪ **rap5*
+║☯︎ ➪ **glub*
+║
+╠══༒︎〘 OUTROS /2 〙༒︎══
+║☯︎ ➪ **bot*
+║☯︎ ➪ **antilink [1/0]*
+║☯︎ ➪ **brainly [pergunta]*
+║☯︎ ➪ **antiracismo [on/off]*
+║☯︎ ➪ **setnomebot*
+║☯︎ ➪ **meme*
+║
+╠══༒︎〘 INTERATIVOS 〙༒︎══
+║
+╠══NOTA »
+║Mandar a msg sem o prefixo
+╠════════════════════
+║
+║☯︎ ➪ *bah*
+║☯︎ ➪ *oii*
+║☯︎ ➪ *bv*
+║☯︎ ➪ *canta ai bot*
+║☯︎ ➪ *grita*
+║☯︎ ➪ *bot lixo*
+║
+╠══༒︎〘 XANDÃO NO CONTROLE 〙༒︎══
+║ 
+║Numero do Causs: wa.me/5547992091566
+║Se ele não responder manda dnv pq ele e ocupado só nao liga porra
+╚═〘 Xandão Bot 〙`
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var Horas = Math.floor(seconds / (60*60));
+  var Minutos = Math.floor(seconds % (60*60) / 60);
+  var Segundos = Math.floor(seconds % 60);
+
+  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
+  return `${pad(Horas)} Horas ${pad(Minutos)} Minutos ${pad(Segundos)} Segundos`
+}
+}
+exports.help = help
+
